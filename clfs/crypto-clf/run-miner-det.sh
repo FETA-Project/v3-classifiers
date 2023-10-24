5e7e4459e5eeef74db952f28f9a41f4ada6a15a6
Karel Hynek <hynekkar@fit.cvut.cz>
2023-10-24 14:58:09 +0200

    Creation of miner detector running script

---
+++ clfs/crypto-clf/run-miner-det.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+/usr/bin/decrypto/decrypto -i f:sample_data/mining.trapcap,f:miner_out.trapcap,b:
+$shared_path/clfs/crypto-clf/bin/decrypto/extras/visualizers/detector2txt.py miner_out.trapcap 
