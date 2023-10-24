e1bfb2ce45232dbdd8c6ff5a2c78be7983c1f146
Karel Hynek <hynekkar@fit.cvut.cz>
2023-10-24 15:34:29 +0200

    Add miner detector provision
    Update miner provision and running scripts

---
+++ clfs/crypto-clf/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Running provision script for Miner classifier..."
+
+clf_home="`dirname $0`"
+
+export shared_path="/feta-repo"
+
+for i in `ls $clf_home/rpm/*.rpm`; do
+	dnf install -y --skip-broken $i
+done;
+
+cp run-miner-det.sh $VAGRANT_HOME/run-miner-det.sh
+
+echo "Provision of Miner classifier finished."
